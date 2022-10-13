8aa3b621d44dde83faf8a166fb8a6cc259518852
Thomas Lebegue <thomas.lebegue@gmail.com>
2022-10-13 17:54:22 +0200

    descending order

--- 7-kyu/descending_order.rb
+++ 7-kyu/descending_order.rb
@@ -1,3 +1,5 @@
+# Descending Order
+# Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
 def descending_order(n)
   n.to_s.chars.sort.reverse.join.to_i
 end
