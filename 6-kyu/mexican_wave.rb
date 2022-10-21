c0cb0e6fdce4fdf1a899569decc247ee76085c12
Thomas Lebegue <thomas.lebegue@gmail.com>
2022-10-21 10:28:32 +0200

    mexican wave

---
+++ 6-kyu/mexican_wave.rb
@@ -0,0 +1,21 @@
+# Mexican Wave
+
+# Task
+# In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up.
+# Rules
+#  1.  The input string will always be lower case but maybe empty.
+
+#  2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+# Example
+# wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+
+
+def wave(str)
+  r = []
+
+  str.length.times do | t |
+    next if str[t]==" "
+    r << (t > 0 ? str[0..t-1] : "") + str[t].upcase + str[t+1..str.length]
+  end
+  r
+end
