3cd44cf8cc757bf7eb8f295f49c9ea3d0bf40ab7
Thomas Lebegue <thomas.lebegue@gmail.com>
2022-10-24 17:29:53 +0200

    Decode the Morse code, advanced

---
+++ 8-kyu/convert_a_boolean_to_a_string.rb
@@ -0,0 +1,8 @@
+# Convert a Boolean to a String
+
+# Implement a function which convert the given boolean value into its string representation.
+
+
+def boolean_to_string(b)
+  b ? "true" : "false"
+end
